9579bfa0ff10f3ea0898982000d85e3b9e841305
whl <wplovitz@gmail.com>
2022-12-12 17:42:51 -0800

    added price and stock to product models file

--- models/Product.js
+++ models/Product.js
@@ -17,12 +17,24 @@ Product.init(
       autoIncrement: true,
     },
     product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
       type: DataTypes.DECIMAL(10,2),
       allowNull: false,
       validate: {
         isDecimal: true,
-      }
-    }
+      },
+    },
+    stock: {
+      type: DataTypes.INTEGER, 
+      allowNull: false,
+      references: {
+        model: 'category',
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
