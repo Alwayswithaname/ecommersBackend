181f73ddc3d99ab7644571df2c9837321d53de2c
whl <wplovitz@gmail.com>
2022-12-12 17:07:16 -0800

    added findAll Tags rout, and findOne route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -5,16 +5,39 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all tags
-  // be sure to include its associated Product data
+  Tag.findAll({
+    include: [
+      {
+        module: Product,
+        through: ProductTag,
+      }
+    ]
+  })
+    .then((tags) => res.status(200).json(tags))
+    .catch((err) => res.status(500).json(err));
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
-  // be sure to include its associated Product data
+  Tag.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: [
+      {model: Product, 
+      through: ProductTag,
+    },
+    ],
+  })
+    .then((tag) => res.status(200).json(tag))
+    .catch((err) => res.status(404).json(err));
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create(req.body)
+    .then((tag) => res.status(200).json(tag))
+    .catch((err) => res.status(404).json(err));
 });
 
 router.put('/:id', (req, res) => {
